521211b04b661cf8f79bf7c6672d26c4e0d2e13d
Anthony Bahl <Anthony.R.Bahl@Gmail.com>
2022-12-05 19:51:02 -0700

    Learning about Companion Objects

--- demos/NoClasses/src/main/kotlin/rsk/Person.kt
+++ demos/NoClasses/src/main/kotlin/rsk/Person.kt
@@ -3,6 +3,21 @@ package rsk
 fun main(args: Array<String>) {
     val kevin = Student("Kevin", "Jones", 1, "Some Tutor")
     println(kevin.id)
+
+    Student.createPostgrad("Simon")
+}
+
+// Creating a static main method similar to how a java file would work.
+class Program {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val kevin = Student("Kevin", "Jones", 1, "Some Tutor")
+            println(kevin.id)
+
+            Student.createPostgrad("Simon")
+        }
+    }
 }
 
 abstract class Person(var firstName: String, var lastName: String) {
@@ -11,13 +26,49 @@ abstract class Person(var firstName: String, var lastName: String) {
     abstract fun getAddress():String
 }
 
-class Student(firstName: String, lastName: String, val id: Int) : Person(firstName, lastName) {
+open class Student(firstName: String, lastName: String, _id: Int, var tutor: String = "") : Person(firstName, lastName) {
+    val id : Int
 
-    constructor(firstName: String, lastName: String, id: Int, var tutor: String) : this(firstName, lastName, id) { }
+    init {
+        id = _id
+    }
+
+    fun enrole(courseName : String) {
+        val course = Courses.allCourses
+            .filter { it.Title == courseName }
+            .firstOrNull()
+    }
 
     override fun getAddress(): String {
         TODO("Not yet implemented")
     }
 
     override fun getName() : String {return "$lastName, $firstName"}
+
+    // Factory methods that can be used without creating an instance of a class.
+    // These are like static methods
+    companion object : XmlSerializer<Student> {
+        override fun toXml(item: Student) {
+            TODO("Not yet implemented")
+        }
+
+        fun createUndergrad(name: String): Undergraduate {
+            return Undergraduate(name)
+        }
+        fun createPostgrad(name: String): Postgraduate {
+            return Postgraduate(name)
+        }
+    }
+}
+
+class Undergraduate(firstName:String) : Student(firstName, "", 1) {
+
+}
+
+class Postgraduate(firstName:String) : Student(firstName, "", 1) {
+
+}
+
+interface XmlSerializer<T> {
+    fun toXml(item: T)
 }
